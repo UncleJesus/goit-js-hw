01b35749383e8c9961df549b297d88d47e41e13f
Uncle J <domelyanov@hotmail.com>
2020-07-22 09:26:51 +0300

    с

---
+++ module2/task-2.js
@@ -0,0 +1,143 @@
+// Задание 1
+
+// const logItems = function (array) {
+//   for (let i = 0; i < array.length; i++) {
+//     console.log(i + 1, array[i]);
+//   }
+// };
+// logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
+// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+// \\--------------------------------
+// Задание 2
+// \\--------------------------------
+
+// const calculateEngravingPrice = function (message, pricePerWord) {
+//   const arrayCount = message.split(" ");
+//   return arrayCount.length * pricePerWord;
+// };
+// console.log(
+//   calculateEngravingPrice(
+//     "Proin sociis natoque et magnis parturient montes mus",
+//     10
+//   )
+// ); // 80
+
+// console.log(
+//   calculateEngravingPrice(
+//     "Proin sociis natoque et magnis parturient montes mus",
+//     20
+//   )
+// ); // 160
+
+// console.log(
+//   calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+// ); // 240
+
+// console.log(
+//   calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
+// ); // 120
+
+// \\--------------------------------
+// Задание 3
+// \\--------------------------------
+
+// const findLongestWord = function (string) {
+//   const stringArray = string.split(" ");
+//   let longestWord = stringArray[0];
+
+//   for (let i = 1; i < stringArray.length; i += 1) {
+//     if (stringArray[i].length > longestWord.length) {
+//       longestWord = stringArray[i];
+//     }
+//   }
+
+//   return longestWord;
+// };
+
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
+
+// console.log(findLongestWord("Google do a roll")); // 'Google'
+
+// console.log(findLongestWord("May the force be with you")); // 'force'
+
+// \\--------------------------------
+// Задание 4
+// \\--------------------------------
+
+// const formatString = function (string) {
+//   if (string.length <= 40) {
+//     return string;
+//   }
+//   const newString = string.split("").slice(0, 40).join("") + "...";
+//   return newString;
+// };
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// // вернется оригинальная строка
+
+// console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// // вернется форматированная строка
+
+// console.log(formatString("Curabitur ligula sapien."));
+// // вернется оригинальная строка
+
+// console.log(
+//   formatString(
+//     "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+//   )
+// );
+// // вернется форматированная строка
+
+// \\--------------------------------
+// Задание 5
+// \\--------------------------------
+
+// const checkForSpam = function (message) {
+//   if (
+//     message.toLowerCase().includes("sale") ||
+//     message.toLowerCase().includes("spam")
+//   ) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(checkForSpam("Latest technology news")); // false
+
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+
+// console.log(checkForSpam("Get best sale offers now!")); // true
+
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+// \\--------------------------------
+// Задание 6
+// \\--------------------------------
+
+// let input;
+// const numbers = [];
+// let total = 0;
+
+// while ((input = prompt("Введите число:"))) {
+//   if (isNaN(input)) {
+//     alert("Было введено не число, попробуйте ещё раз");
+//     continue;
+//   }
+//   numbers.push(input);
+// }
+
+// for (const sum of numbers) {
+//   total += Number(sum);
+// }
+
+// console.log(`Общая сумма чисел равна ${total}`);
