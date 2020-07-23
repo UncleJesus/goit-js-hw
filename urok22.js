be11f57ee376870e2ad5329fcf02c86927de1735
Uncle J <domelyanov@hotmail.com>
2020-07-23 23:05:58 +0300

    c

---
+++ urok22.js
@@ -0,0 +1,10 @@
+// const houses = ["Arryn", "Frey", "Greyjoy", "Stark", "Lannister", "Tyrell"];
+
+// // Вертикальная запись удобнее для чтения
+// const updatedHouses = [
+//   ...houses.slice(0, 1),
+//   "Frey of the Crossing",
+//   ...houses.slice(2),
+// ];
+
+// console.log(updatedHouses);
